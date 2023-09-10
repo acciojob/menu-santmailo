a9f8787d4cdf521e952fce22b4c51acb0353f21c
santmailo <iamsunil006@gmail.com>
2023-09-10 23:28:17 +0530

    some minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useState} from 'react';
 
+const App = ({dishes}) => {
+    return (
+        <div>
+            <h1>This is a component</h1>
+        </div>
+    )
+}
+
+export default App;
