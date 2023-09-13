7e377995a3342b4c65538559cd07e5d5ef266741
santmailo <iamsunil006@gmail.com>
2023-09-13 21:18:10 +0530

    done solving assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import ItemBlock from './itemBlock';
 
 const App = ({dishes}) => {
     const [menuItem, setMenuItem] = useState(dishes);
+    const [currentTab, setCurrentTab] = useState({breakfast: true, lunch: true, shakes: true});
+
+
     const items = menuItem.map((item)=> {
         return <ItemBlock key={item.id} item = {item} />
     });
@@ -19,6 +22,7 @@ const App = ({dishes}) => {
                 return item.category==="breakfast";
             })
             handleMenuChange(tempitems);
+            setCurrentTab({breakfast: true, lunch: false, shakes: false});
             return;
         }
         
@@ -27,6 +31,7 @@ const App = ({dishes}) => {
                 return item.category==="lunch";
             })
             handleMenuChange(tempitems);
+            setCurrentTab({breakfast: false, lunch: true, shakes: false});
             return;
         }
         
@@ -35,12 +40,14 @@ const App = ({dishes}) => {
                 return item.category==="shakes";
             })
             handleMenuChange(tempitems);
+            setCurrentTab({breakfast: false, lunch: false, shakes: true});
             return;
         }
         
         
         else{
             handleMenuChange(dishes);
+            setCurrentTab({breakfast: true, lunch: true, shakes: true});
             return;
         }
 
@@ -51,9 +58,9 @@ const App = ({dishes}) => {
             <h1>Our Menu</h1>
             <div className='button-blocks'>
                 <button onClick={handleClick} value="all">All</button>
-                <button id="filter-btn-1" onClick={handleClick} value="breakfast" data-test-id="menu-item-breakfast">Breakfast</button>
-                <button id="filter-btn-2"onClick={handleClick} value="lunch" data-test-id="menu-item-lunch">Lunch</button>
-                <button id="filter-btn-3" onClick={handleClick} value="shakes" data-test-id="menu-item-shakes">Shakes  </button>
+                 {currentTab.breakfast && <button id="filter-btn-1" onClick={handleClick} value="breakfast" data-test-id="menu-item-breakfast">Breakfast</button>}
+                 {currentTab.lunch && <button id="filter-btn-2"onClick={handleClick} value="lunch" data-test-id="menu-item-lunch">Lunch</button>}
+                 {currentTab.shakes && <button id="filter-btn-3" onClick={handleClick} value="shakes" data-test-id="menu-item-shakes">Shakes  </button>}
             </div>
 
             <div className='items-block'>
